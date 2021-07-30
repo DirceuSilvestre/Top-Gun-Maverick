b9183389da22daf31d6f4c9a435b8c2b86e8a6c8
Dirceu Silvestre <dirceu_silvestre@hotmail.com>
2021-07-30 17:34:47 -0300

    encerramento

---
+++ endgame.c
@@ -0,0 +1,322 @@
+void endgame(SDL_Window *janela, SDL_Renderer *renderer)
+{
+	final[0] = setElement(
+        "final[0]",
+        IMG_Load("png/final1.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[1] = setElement(
+        "final[1]",
+        IMG_Load("png/final2.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[2] = setElement(
+        "final[2]",
+        IMG_Load("png/final3.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[3] = setElement(
+        "final[3]",
+        IMG_Load("png/final4.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[4] = setElement(
+        "final[4]",
+        IMG_Load("png/final5.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[5] = setElement(
+        "final[5]",
+        IMG_Load("png/final6.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[6] = setElement(
+        "final[6]",
+        IMG_Load("png/final7.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[7] = setElement(
+        "final[7]",
+        IMG_Load("png/final8.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[8] = setElement(
+        "final[8]",
+        IMG_Load("png/final9.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[9] = setElement(
+        "final[9]",
+        IMG_Load("png/final10.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[10] = setElement(
+        "final[10]",
+        IMG_Load("png/final11.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[11] = setElement(
+        "final[11]",
+        IMG_Load("png/final12.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[12] = setElement(
+        "final[12]",
+        IMG_Load("png/final13.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[13] = setElement(
+        "final[13]",
+        IMG_Load("png/final14.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[14] = setElement(
+        "final[14]",
+        IMG_Load("png/final15.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[15] = setElement(
+        "final[15]",
+        IMG_Load("png/final16.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[16] = setElement(
+        "final[16]",
+        IMG_Load("png/final17.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[17] = setElement(
+        "final[17]",
+        IMG_Load("png/final18.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[18] = setElement(
+        "final[18]",
+        IMG_Load("png/final19.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[19] = setElement(
+        "final[19]",
+        IMG_Load("png/final20.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[20] = setElement(
+        "final[20]",
+        IMG_Load("png/final21.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[21] = setElement(
+        "final[21]",
+        IMG_Load("png/final22.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[22] = setElement(
+        "final[22]",
+        IMG_Load("png/final23.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[23] = setElement(
+        "final[23]",
+        IMG_Load("png/final24.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[24] = setElement(
+        "final[24]",
+        IMG_Load("png/final25.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[25] = setElement(
+        "final[25]",
+        IMG_Load("png/final26.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[26] = setElement(
+        "final[26]",
+        IMG_Load("png/final27.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[27] = setElement(
+        "final[27]",
+        IMG_Load("png/final28.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[28] = setElement(
+        "final[28]",
+        IMG_Load("png/final29.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[29] = setElement(
+        "final[29]",
+        IMG_Load("png/final30.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[30] = setElement(
+        "final[30]",
+        IMG_Load("png/final31.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[31] = setElement(
+        "final[31]",
+        IMG_Load("png/final32.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[32] = setElement(
+        "final[32]",
+        IMG_Load("png/final33.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[33] = setElement(
+        "final[33]",
+        IMG_Load("png/final34.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[34] = setElement(
+        "final[34]",
+        IMG_Load("png/final35.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[35] = setElement(
+        "final[35]",
+        IMG_Load("png/final36.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[36] = setElement(
+        "final[36]",
+        IMG_Load("png/final37.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[37] = setElement(
+        "final[37]",
+        IMG_Load("png/final38.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[38] = setElement(
+        "final[38]",
+        IMG_Load("png/final39.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[39] = setElement(
+        "final[39]",
+        IMG_Load("png/final40.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[40] = setElement(
+        "final[40]",
+        IMG_Load("png/final41.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[41] = setElement(
+        "final[41]",
+        IMG_Load("png/final42.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[42] = setElement(
+        "final[42]",
+        IMG_Load("png/final43.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[43] = setElement(
+        "final[43]",
+        IMG_Load("png/final44.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    final[44] = setElement(
+        "final[44]",
+        IMG_Load("png/final45.png"),
+        createRect(0,0,1920,1080),   //cut: x, y, w, h
+        createRect(0,0,WIDTH,HEIGHT) //sizeImg: x, y, w, h
+    );
+    int a = 0;
+
+    playSong(15);
+    while(1)
+    {
+        SDL_Event event;
+        while(SDL_PollEvent(&event))
+        {
+
+            switch(event.type)
+            {
+                case SDL_QUIT:
+                    SDL_DestroyRenderer(renderer);
+                    SDL_DestroyWindow(janela);
+                    SDL_Quit();
+                    return;
+                    break;
+
+                case SDL_KEYDOWN:
+                    switch(event.key.keysym.scancode)
+                    {
+                        case SDL_SCANCODE_ESCAPE:
+                            playSound(0);
+                            gameTitle(janela, renderer);
+                            return;
+                            break;
+
+                        case SDL_SCANCODE_RETURN:
+                        	playSound(0);
+                            gameTitle(janela, renderer);
+                            return;
+                            break;
+
+                    }
+                    break;
+
+            }
+        }
+
+        a++;
+        if(a>=45){a= 0;}
+
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, final[a].Texture, &final[a].cut, &final[a].sizeImg);
+        SDL_RenderPresent(renderer);
+        SDL_Delay(150);
+
+    }
+    return;
+}
